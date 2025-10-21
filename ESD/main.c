d4344615b15f4842ee9024c1e18791cb6186f417
Abhishek Jadhav <76618289+AbhishekJadhav05@users.noreply.github.com>
2025-10-21 12:59:06 +0530

    Integrate ADC for temperature detection and update LCD
    Added ADC functionality to read temperature and updated LCD messages accordingly.

--- ESD/main.c
+++ ESD/main.c
@@ -1,19 +1,26 @@
 #include <LPC17xx.h>
+#include <stdio.h>
 
 #define DT_CTRL  0x000000F0     // P0.4–P0.7 (LCD D4–D7)
 #define RS_CTRL  (1 << 8)       // P0.8 (RS)
 #define EN_CTRL  (1 << 9)       // P0.9 (EN)
-#define LED_PIN  (1 << 11)      // P0.11 (LED output)
 #define PIR_PIN  (1 << 15)      // P0.15 (PIR input)
+#define ADC_CHANNEL 0
+#define TEMP_THRESHOLD 372  // corresponds to ~30°C
+
 
 void lcd_write(void);
 void port_write(void);
 void delay_lcd(unsigned int);
 
-char msg_on1[]  = "LED:ON ";
-char msg_on2[]  = "OBJECT:YES";
-char msg_off1[] = "LED:OFF";
-char msg_off2[] = "OBJECT:NO ";
+void ADC_Init(void);
+uint16_t ADC_Read(void);
+void delay_ms(uint32_t ms);
+
+
+char msg_on1[]  = "INTRUSION: ";
+char msg_values[15];
+char msg_off1[] = "DETECTING: ";
 
 unsigned long int init_command[] = {0x33,0x32,0x28,0x0C,0x06,0x01,0x80};
 unsigned long int temp1, temp2;
@@ -21,8 +28,10 @@ unsigned int i, flag1;
 
 int main(void)
 {
-    unsigned long int x;
-
+    int ifDetectedPIR;
+	  int ifDetectedTEMP;
+		uint16_t adc_value;
+	
     SystemInit();
     SystemCoreClockUpdate();
 
@@ -30,10 +39,7 @@ int main(void)
     LPC_PINCON->PINSEL0 = 0;   // P0.0–P0.15 as GPIO
     LPC_PINCON->PINSEL1 = 0;
 
-    LPC_GPIO0->FIODIR |= (DT_CTRL | RS_CTRL | EN_CTRL | LED_PIN); // outputs
     LPC_GPIO0->FIODIR &= ~PIR_PIN;  // PIR input
-
-    LPC_GPIO0->FIOCLR = LED_PIN;    // LED off initially
 
     // LCD Initialization
     flag1 = 0;
@@ -42,43 +48,47 @@ int main(void)
         lcd_write();
         delay_lcd(30000);
     }
+		ADC_Init();
 
-    // Initial message: LED OFF / OBJECT NO
+    // Initial message
     flag1 = 1;
     for (i = 0; msg_off1[i] != '\0'; i++) { temp1 = msg_off1[i]; lcd_write(); }
-    flag1 = 0; temp1 = 0xC0; lcd_write();
-    flag1 = 1;
-    for (i = 0; msg_off2[i] != '\0'; i++) { temp1 = msg_off2[i]; lcd_write(); }
 
     // Main loop
     while (1)
     {
-        x = LPC_GPIO0->FIOPIN & PIR_PIN;
-        if (x) // Motion detected
+				adc_value = ADC_Read();
+				if(adc_value > TEMP_THRESHOLD){
+					ifDetectedTEMP = 1;
+				}
+				else{
+					ifDetectedTEMP = 0;
+				}
+        ifDetectedPIR = LPC_GPIO0->FIOPIN & PIR_PIN;
+        if (ifDetectedTEMP & ifDetectedPIR) // Motion detected
         {
-            LPC_GPIO0->FIOSET = LED_PIN;
 
-            // Update LCD: LED ON / OBJECT YES
+            // Update LCD: INTRUSION!!!!!!!!!!
             flag1 = 0; temp1 = 0x80; lcd_write();
             flag1 = 1;
             for (i = 0; msg_on1[i] != '\0'; i++) { temp1 = msg_on1[i]; lcd_write(); }
 
             flag1 = 0; temp1 = 0xC0; lcd_write();
             flag1 = 1;
-            for (i = 0; msg_on2[i] != '\0'; i++) { temp1 = msg_on2[i]; lcd_write(); }
+						sprintf(msg_values,"TEMP %d PIR %d",adc_value,ifDetectedPIR);
+            for (i = 0; msg_values[i] != '\0'; i++) { temp1 = msg_values[i]; lcd_write(); }
         }
         else // No motion
         {
-            LPC_GPIO0->FIOCLR = LED_PIN;
-
-            // Update LCD: LED OFF / OBJECT NO
+            // Update LCD: DETECTING < values
             flag1 = 0; temp1 = 0x80; lcd_write();
             flag1 = 1;
             for (i = 0; msg_off1[i] != '\0'; i++) { temp1 = msg_off1[i]; lcd_write(); }
 
             flag1 = 0; temp1 = 0xC0; lcd_write();
             flag1 = 1;
-            for (i = 0; msg_off2[i] != '\0'; i++) { temp1 = msg_off2[i]; lcd_write(); }
+						sprintf(msg_values,"TEMP %d PIR %d",adc_value,ifDetectedPIR);
+            for (i = 0; msg_values[i] != '\0'; i++) { temp1 = msg_values[i]; lcd_write(); }
         }
         delay_lcd(50000);
     }
@@ -112,6 +122,29 @@ void port_write(void)
     delay_lcd(10000);
     LPC_GPIO0->FIOCLR = EN_CTRL;
     delay_lcd(10000);
+}
+
+void ADC_Init(void) {
+    LPC_PINCON->PINSEL1 &= ~(3 << 14);
+    LPC_PINCON->PINSEL1 |=  (1 << 14);   // P0.23 as AD0.0
+    LPC_SC->PCONP |= (1 << 12);          // Power up ADC
+    LPC_ADC->ADCR = (1 << ADC_CHANNEL) | // Select AD0.0
+                    (4 << 8) |           // ADC clock = PCLK/5
+                    (1 << 21);           // Enable ADC
+}
+
+uint16_t ADC_Read(void) {
+		uint16_t result;
+    LPC_ADC->ADCR |= (1 << 24);          // Start conversion
+    while ((LPC_ADC->ADGDR & (1U << 31)) == 0); // Wait for DONE
+    result = (LPC_ADC->ADGDR >> 4) & 0xFFF; // 12-bit result
+    return result;
+}
+
+void delay_ms(uint32_t ms) {
+    uint32_t i, j;
+    for (i = 0; i < ms; i++)
+        for (j = 0; j < 5000; j++);
 }
 
 void delay_lcd(unsigned int r1)
